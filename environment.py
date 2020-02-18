78500a28c792d6adb111cf1be6b2a7bf96f56ad8
Maksym Bevza <maksymbevza@gmail.com>
2020-02-18 11:17:51 +0200

    Make env training (badly)

---
+++ environment.py
@@ -0,0 +1,111 @@
+import gym
+import numpy as np
+from gym import spaces
+
+
+DENOMINATOR = 100.0
+EPS = 1e-9
+
+
+class MachineProductionEnv(gym.Env):
+
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self,
+                 machine_detail_time,
+                 detail_tree):
+
+        super(MachineProductionEnv, self).__init__()
+        self.machine_detail_time = machine_detail_time / DENOMINATOR
+        self.detail_tree = detail_tree / DENOMINATOR
+
+        self.d_count = machine_detail_time.shape[0]
+        self.m_count = machine_detail_time.shape[1]
+        self.action_space = spaces.MultiDiscrete([self.d_count + 1]*self.m_count)
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=1,  #TODO: Fix high to dynamic
+            shape=(self.d_count*2 + self.m_count,),
+            dtype=int)
+
+    def reset(self):
+        # Reset the state of the environment to an initial state
+        self.current_step = 0
+        self.produced = np.zeros((self.d_count), dtype=np.float32)
+        self.needed = np.zeros((self.d_count), dtype=np.float32)
+        self.scheduled_for = np.zeros((self.m_count), dtype=np.float32)
+        self.detail_line = []
+        self.machine_busy = np.zeros((self.m_count,), dtype=np.float32)
+
+        self.needed[0] = 1.0/DENOMINATOR  # TODO: Read this from file
+        self.cumulative_reward = 0
+
+        return self._next_observation()
+
+    def _next_observation(self):
+        obs = np.concatenate([self.produced,
+                              self.needed,
+                              self.machine_busy])
+
+        return obs
+
+    def step(self, action):
+        reward = -5  # Penalty for time
+        reward += self._take_action(action)
+        self._produce()
+
+        self.current_step += 1
+
+        done = (self.needed - EPS <= self.produced).all()
+        if done:
+            reward += 1000
+        obs = self._next_observation()
+        self.cumulative_reward += reward
+        return obs, reward, done, {"cumulative_reward": self.cumulative_reward}
+
+    def _take_action(self, action):
+        reward = 0
+        for machine_id, detail_id in enumerate(action):
+            if detail_id == 0:
+                continue
+            detail_id -= 1
+            if self.machine_busy[machine_id] > EPS:
+                reward -= 1
+                continue
+            if (self.detail_tree[detail_id] > self.produced).any():
+                reward -= 1
+                continue
+
+            self.produced -= self.detail_tree[detail_id]
+
+#            reward += 1
+            ttc = self.machine_detail_time[detail_id, machine_id]
+            self.machine_busy[machine_id] = 1
+            self.scheduled_for[machine_id] = ttc
+            self.detail_line.append([ttc,
+                                     detail_id,
+                                     machine_id])
+        return reward
+
+    def _produce(self):
+        self.scheduled_for -= self.machine_busy / DENOMINATOR
+
+        new_detail_line = []
+        for ttc, detail_id, machine_id in self.detail_line:
+            if (ttc - 1/DENOMINATOR) < EPS:
+                self.machine_busy[machine_id] = 0
+                self.produced[detail_id] += 1 / DENOMINATOR
+            else:
+                new_detail_line.append([ttc-1/DENOMINATOR, detail_id, machine_id])
+
+        self.detail_line = new_detail_line
+
+    def render(self, mode='human', close=False):
+        print(f'Step: {self.current_step}')
+        print(f'Produced: {self.produced}')
+        print(f'Needed: {self.needed}')
+        print(f'Scheduled For: {self.scheduled_for}')
+        print(f'Machine Busy: {self.machine_busy}')
+        print(f'Detail Line: {self.detail_line}')
+        print(f'Cumulative Reward: {self.cumulative_reward}')
--- pendulum.py
+++ pendulum.py
@@ -15,80 +15,40 @@ import tensorflow as tf
 from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
 import multiprocessing
 
+from environment import MachineProductionEnv
 
-def train():
+
+def make_env():
+    from lib import read_detail_tree, read_machine_detail
+    machine_detail = read_machine_detail("data/machine_detail.txt")
+    detail_tree = read_detail_tree("data/detail_tree.txt",
+                                   machine_detail.shape[0])
+    env = MachineProductionEnv(machine_detail, detail_tree)
+    return env
+
+
+def play(train=True):
     ncpu = 1
     config = tf.ConfigProto(allow_soft_placement=True,
                             intra_op_parallelism_threads=ncpu,
                             inter_op_parallelism_threads=ncpu)
     tf.Session(config=config).__enter__()
 
-    env = DummyVecEnv([lambda: gym.make('Pendulum-v0')])
+    env = DummyVecEnv([make_env])
 
     env = VecNormalize(env)
     seed = 10
     set_global_seeds(seed)
-    policy = MlpPolicy
-    model = PPO2(policy=policy, 
-                 env=env,
-                 n_steps=2048,
-                 nminibatches=32,
-                 noptepochs=10,
-                 #n_steps=32, 
-                 #nminibatches=4,
-                 lam=0.95, 
-                 gamma=0.995, 
-                 #noptepochs=4,
-                 tensorboard_log="tb_log",
-                 #log_interval=1,
-                 #tb_log_interval=20,
-                 ent_coef=0.0,
-                 learning_rate=3e-4,
-                 cliprange=0.2)
-    for i in range(1):
-        mult = int(1e5)
-        model.learn(total_timesteps=mult)
-        model.save(f"models/PPO2_model_{i*mult:15d}.bin")
-
-    return model, env
-
-
-def play_random():
-    import gym
-    env = gym.make('Pendulum-v0')
-    obs = env.reset()
-    running_reward = 0.0
-    alpha = 0.01
-    for _ in range(100):
-        env.render()
-        action = env.action_space.sample()
-        obs, reward, done, info = env.step(action)
-        running_reward = running_reward * (1-alpha) + alpha * reward
-        print(obs, reward, done, info, running_reward)
-        if done:
-            print("Finished after {} timesteps".format(t+1))
-            break
-
-
-def play(model="", train=True):
-    import gym
-    seed = 10
-    set_global_seeds(seed)
-
-    env = gym.make('LunarLander-v2')
-    env = DummyVecEnv([lambda: env])
-
-    policy = MlpPolicy
     model = PPO2(policy="MlpPolicy",
                 env=env,
                 tensorboard_log="tb_log",
                 ent_coef=0.0,
-                n_steps=500,
-                nminibatches=10,
+                n_steps=300,
+                nminibatches=20,
                 noptepochs=10,
                 learning_rate=0.0003,
                 cliprange=0.2,
-                gamma=0.998,
+                gamma=0.99,
                 verbose=1)
 
     def test(model):
@@ -103,17 +63,17 @@ def play(model="", train=True):
                 env.render()
                 #running_reward = running_reward * (1-alpha) + alpha * reward
                 running_reward += reward
-                print(obs, reward, done, info, running_reward)
+                #print(obs, reward, done, info, running_reward)
                 if done:
                     print("Finished after {} timesteps".format(_+1))
                     break
 
     if train:
         for i in range(1):
-            model.learn(total_timesteps=1000000, log_interval=10)
-            model.save(f'models/lunar_{i}.bin')
+            model.learn(total_timesteps=1000_000, log_interval=10)
+            model.save(f'models/machine_{i}.bin')
 
-    model = PPO2.load('models/lunar_0.bin')
+    #model = PPO2.load('models/machine_.bin')
     test(model)
 
 
@@ -121,6 +81,7 @@ def main():
     logger.configure()
     #train()
     play(train=True)
+
 
 if __name__ == '__main__':
     main()
